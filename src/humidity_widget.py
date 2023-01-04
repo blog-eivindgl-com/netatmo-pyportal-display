dcf0589661ca227f5e4321b227e8398ceec635a5
Eivind Gussiås Løkseth <eloekset@msn.com>
2023-01-04 23:54:52 +0100

    Revert back to full scale weather icons

--- src/humidity_widget.py
+++ src/humidity_widget.py
@@ -7,20 +7,20 @@ class Humidity_Widget(displayio.Group):
         self.battery_level = 100
         
         # day weather
-        self._day_sunny = cwd + "/icons/wi-day-sunny-60x60.bmp";
-        self._day_sunny_overcast = cwd + "/icons/wi-day-sunny-overcast-60x60.bmp";
-        self._day_cloudy = cwd + "/icons/wi-day-cloudy-60x60.bmp";
-        self._day_sprinkle = cwd + "/icons/wi-day-sprinkle-60x60.bmp";
-        self._day_showers = cwd + "/icons/wi-day-showers-60x60.bmp";
-        self._day_rain = cwd + "/icons/wi-day-rain-60x60.bmp";
-        self._day_sleet = cwd + "/icons/wi-day-sleet-60x60.bmp";
-        self._day_snow = cwd + "/icons/wi-day-snow-60x60.bmp";
+        self._day_sunny = cwd + "/icons/wi-day-sunny.bmp";
+        self._day_sunny_overcast = cwd + "/icons/wi-day-sunny-overcast.bmp";
+        self._day_cloudy = cwd + "/icons/wi-day-cloudy.bmp";
+        self._day_sprinkle = cwd + "/icons/wi-day-sprinkle.bmp";
+        self._day_showers = cwd + "/icons/wi-day-showers.bmp";
+        self._day_rain = cwd + "/icons/wi-day-rain.bmp";
+        self._day_sleet = cwd + "/icons/wi-day-sleet.bmp";
+        self._day_snow = cwd + "/icons/wi-day-snow.bmp";
 
         # night weather
 
         self._weather_icon_sprite = None
         self._weather_icon_file = None
-        self._weather_icon_group = displayio.Group(x=0, y=0, scale=4)
+        self._weather_icon_group = displayio.Group(x=0, y=0, scale=1)
         self.append(self._weather_icon_group)
         self.set_icon(self._day_sunny)
     
